b5ece8828ad7267f50ac16580fb2d9c4b2485cc1
Mike Fomin <mikefomin@Mikes-MacBook-Pro.local>
2022-10-11 17:47:41 -0700

    made a variable for the questions and put questions in them

---
+++ assests/js/script.js
@@ -0,0 +1,17 @@
+var questionsArray = [
+    {
+        questionNumber: 1,
+        questionTitle: 'What is the capital of California ? ',
+        correctAnswer: 'Sacromento',
+        possibleAnswers: ['Chico', 'San Fransisco', 'Uc Davic', 'Sacromento']
+
+    },
+
+    {
+        questionNumber: 2,
+        questionTitle: 'What color is the sky? ',
+        correctAnswer: 'Blue',
+        possibleAnswers: ['Red', 'Blue', 'Orange', 'Green']
+
+    }
+];
